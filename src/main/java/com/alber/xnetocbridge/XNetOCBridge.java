d29b6e68e62f61e37a67397993528c4bc49dcfa0
Rico-Grant <albert.ex@outlook.com>
2026-07-14 13:55:32 +0800

    Refine OC indicator letters

--- src/main/java/com/alber/xnetocbridge/XNetOCBridge.java
+++ src/main/java/com/alber/xnetocbridge/XNetOCBridge.java
@@ -31,7 +31,7 @@ public class XNetOCBridge {
 
     public static final String MODID = "xnetocbridge";
     public static final String MODNAME = "XNet OC Bridge";
-    public static final String VERSION = "1.3.17";
+    public static final String VERSION = "1.3.18";
 
     public static final Logger LOGGER = LogManager.getLogger(MODNAME);
 
